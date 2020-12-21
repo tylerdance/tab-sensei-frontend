904b72411a0afda3cfbc8fd0241d5ec51a60d030
Lev Michael <lev.choubine@gmail.com>
2020-12-20 19:46:06 -0500

    [BeatBank]

--- src/components/BeatBank.js
+++ src/components/BeatBank.js
@@ -1,1 +1,14 @@
+console.log('タイラ');
+console.log('レヴ');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
+console.log('hi');
 console.log('hi');
