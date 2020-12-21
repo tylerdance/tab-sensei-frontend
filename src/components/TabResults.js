4c5fb5ea620b6a78ae07b6c334d17f8e59ad4e4a
Lev Michael <lev.choubine@gmail.com>
2020-12-21 16:33:08 -0500

    cors-issue fixed

--- src/components/TabResults.js
+++ src/components/TabResults.js
@@ -1,10 +1,16 @@
 function TabResults (props){
-    const starShips = props.starShipList.map((p, index)=>{
-       const link = `http://www.songsterr.com/a/wa/song?id=${p.id}`
+    const starShips = props.tabs.map((p, index)=>{
+    const handleClick=(e)=>{
+        
+        console.log(e.target.value)
+    }
+
+       const link = `http://www.songsterr.com/a/wa/song?id=${p.title}`
         
         return <div className="starShips" key={index}>
-               <h2> Artist: {p.artist.name}</h2>
-               <a className="link" href={link} target="_blank" rel="noreferrer">{p.title}</a>
+               <h2> Artist: {p.title}</h2>
+               <a className="link" href={link} target="_blank" rel="noreferrer">Hey</a>
+               <button type="button"  onClick={handleClick}>Save Tab!</button >
               </div>
     })
     return(
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,80 +1,68 @@
-import React, { useEffect} from 'react'
-import { useState} from 'react';
-import axios from 'axios';
-import { BrowserRouter, Route, Redirect} from 'react-router-dom';
 
-
-//////---COMPONENTS BELLOW
+import React, { Component } from 'react';
+import Axios from 'axios'; 
+import { Link } from 'react-router-dom';
+// import '../home.css';
 import TabResults from './TabResults';
-import Header from './Header.js'
-import LoadingPage from './LoadingPage'
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
+class Tabs extends Component {
+    constructor() {
+        super()
 
-//////---COUNTERS AND DATA STORAGE
-let userInput = ""
-// let count = 0;
-function Tabs() {
-  // const [starshipArray, setStarshipArray] = useState([]);
-function getTabs(){
-    axios({method: 'get',
-    headers: { 'Content-Type': 'application/json'},
-        url: userUrl,
+        this.state = {
+            starships: [],
+        }
+    }
+
+    async componentDidMount() {
+       
+        const res = await Axios.get(`${REACT_APP_SERVER_URL}/api/request/test`)
+        console.log('successful');
+        console.log(res.data);
+       
+        
+        this.setState({
+            starships: res.data
         })
-  .then(async(result) => {
-    console.log('!requestmade!')
-    await setStarshipArray(result.data)
-    await setLoading(false)
-    // console.log('!!!!!!!!!!$$$$$$$'+ JSON.stringify(result.data))
-    // console.log('state police   ' + userUrl)
-    // count = count+1;
-  }).catch((err)=>{console.log(err+'Second BLOCKKKKKKKKKKKKKKKKK')})
-}
-/////Setting our state here:
-  const [starshipArray, setStarshipArray] = useState([]);
-  const [loading, setLoading] = useState(true)
-  const [userUrl, setUserUrl] = useState(`https://cors-anywhere.herokuapp.com/http://www.songsterr.com/a/ra/songs.json?pattern=${userInput}`)
- 
-  ///////function that will make API call to fetch results after the information is enetered into the searchbar
-  const searchBar =async(e)=>{
-    e.preventDefault()
-    // console.log("getting tabs")
-    getTabs()
-    return <Redirect to="/profile" />
-  }
-    // console.log('get tabs from' + userUrl)
- ////////////////////TABS component   
-  useEffect(()=>{
-        getTabs()
-    },[]) 
-  ///////////////////////implements LOADING PAGE and TABS comp
-  return (
-    <>
-    { loading ===false ? (
-    <BrowserRouter>
-    <div className="App">
-    <Header /> 
-   
-    <form onSubmit={searchBar}   >
-      <label>Search for Tabs</label> 
-      <br/>     
-      <input id="inputSearchbar" type="text" onChange={(e => {setUserUrl(`https://cors-anywhere.herokuapp.com/http://www.songsterr.com/a/ra/songs.json?pattern=${e.target.value}`)})}  ></input>
-      <br/>  
-      <input id="submit" type="submit" value="getTabs" ></input>
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+    if (this.state.starships === '') {
+        return;
+    }
+    if (prevState.starships === this.state.starshipName) {
+        return;
+    }
 
-    </form>  
-    <Route path="/profile" render={()=>{return <TabResults starShipList={starshipArray}/>}} />
-  
-    </div>
-    </BrowserRouter> ) : (
-      <div>
-       <Header />   
-    <LoadingPage />
+    Axios.get(`${REACT_APP_SERVER_URL}/api/request/test`).then((res) => {
+        // console.log("!!!!!" + res)
+    }).catch(err => {
+        console.log(err);
+    })
+    }
 
-    </div>
-
-    )}
-    </>
-  );
+    render() {
+        return(
+            <div className="App">
+              <h2 id="header">Starships</h2>
+              <div id="starships">
+                  {/* {this.state.starships.map((starship, index) => */}
+                  <TabResults tabs={this.state.starships}/>
+                        {/* // <Link id="starship-link"
+                        // key={starship.name}
+                        // to={{
+                        //     pathname: '/starship',
+                        //     state: starship
+                        // }}
+                        // >
+                        // {starship.name}
+                        // </Link>
+                  )} */}
+              </div>
+          </div>
+        )
+    }
 }
 
 export default Tabs;
