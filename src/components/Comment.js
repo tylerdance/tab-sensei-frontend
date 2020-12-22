fc23771b6567b756ef94712d2f906d6789d94ce2
Lev Michael <lev.choubine@gmail.com>
2020-12-22 17:38:40 -0500

    showing comments in the console partially

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,8 +1,39 @@
+import Axios from 'axios'
+import { useState, useEffect } from 'react';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 function Comment (props){
-    const here = props.comment[0].content
+   
+   const  [comments, setComments] = useState([])
+
+
+
+    async function getComments  () {
+        let url =`${REACT_APP_SERVER_URL}/api/users/tabs/${props.songId}`
+         Axios.get(url).then( 
+            async (res)=>{
+                if(res!== undefined){ 
+                     await setComments(res.data.user[0].userProfile[0].comments[0].content) 
+                      
+            }else{
+                const placeHolder = 'be the first one to leave a comment';
+                setComments([{content:placeHolder}])
+            }
+        }).catch(err=>{console.log(err)})
+            
+    }
+
+    useEffect(()=>{  
+       
+        getComments()  
+        console.log(comments) 
+        
+    },[])
+    
+   
+  
     return(
         <div>
-          {here}
+          <h1>{comments}</h1>
         </div>
     )
 }
--- src/components/TabResults.js
+++ src/components/TabResults.js
@@ -1,59 +1,23 @@
 import Axios from 'axios';
 import Comment from './Comment'
+import { useState} from 'react';
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 
 
 function TabResults (props){
+   
     const starShips = props.tabs.map((p, index)=>{
         const link = `http://www.songsterr.com/a/wa/song?id=${p.id}`
         const handleClick=(e)=>{
             console.log("You are saving tab number "+e.target.value)
         }
-   
-    let commentsArray =[]   
-    let comments='op';
-    async function getComments  () {
-        let url =`${REACT_APP_SERVER_URL}/api/users/tabs/${p.id}`
-         Axios.get(url).then( 
-            res=>{
-                if(res!== undefined){
-                    commentsArray = res.data.user[0].userProfile[0].comments; 
-                    console.log(`results for ${p.id}`)
-                    console.log(commentsArray)
-                    comments = commentsArray.map((b, index)=>{
-                         <h2> {b.content}</h2>
-                      
-                         
-                    })
-                  return commentsArray;  
-                }else{
-                    
-                }
-            }
-              ).catch(err=>{console.log(err)})
-            
-    }
 
-     getComments()  
-   
-
-       
-   
-          
             return <div className="starShips" key={index}>
             <h2> Artist: {p.artist.name}</h2>
             <a className="link" href={link} target="_blank" rel="noreferrer">{p.title}</a>
             <button type="button"  value={p.id} onClick={handleClick}>Save Tab!</button >
-            <>
-            {commentsArray.length!==0 ? (
-               <Comment comment={commentsArray}/>
-            ): (
-                <h2>Be the first to leave a comment</h2>
-            )
-               
-
-          }</>
+            <Comment songId={p.id} />
            </div>
     
            
