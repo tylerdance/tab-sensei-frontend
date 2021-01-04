e135cdf7e7f8d940b6627e43bf6bcdb1eadddaa3
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-01-04 17:33:37 -0500

    comments and tabs final updates

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,90 +1,77 @@
 import Axios from 'axios'
 import { useState, useEffect } from 'react';
-import moment from 'moment';
 
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-function clearComment(e) {
-    e.target.value=''
-}
-
 function Comment (props){
-   const [comments, setComments] = useState([])
-   const [commentsStore, setCommentsStore] = useState('')
-   const [clearCommentField, setClearCommentField] = useState('')
-  
-
-
-    // save comments
+    const  [comments, setComments] = useState([])
+    const [commentsStore, setCommentsStore] = useState('')
     async function saveComment(){
-        if(props.email===null){
-            console.log("Please Log In To Save Tabs")
-            alert('Must Be Logged In to Comment')
-        }
-        console.log(commentsStore)
-        console.log(props.songId)
-        console.log(props.email)
-        console.log( moment(Date.now()).format())
-        const userData = {
-                email: props.email,
-                tab_id:  props.songId,
-                content: commentsStore,
-                time: Date.now(), 
-                email: props.email
-        }
-        // if(commentsStore===""){
-        //     return
-        // }
-        await Axios.post(`${REACT_APP_SERVER_URL}/api/users/tabs/comments`, userData)
-        .then(res=>{console.log(res); getComments();})
-        .catch(err=>{console.log(err)})
+    if(props.email===null){
+        // console.log("Please Log In To Save Tabs")
+        alert('Must Be Logged In to Comment')
+    }
+        //  console.log(commentsStore)
+        //  console.log(props.songId)
+        //  console.log(props.email)
+        //  console.log( moment(Date.now()).format())
+         const userData = {
+             email: props.email,
+             tab_id:  props.songId,
+             content: commentsStore,
+             time: Date.now(), 
+             email: props.email
+         }
+         if(commentsStore===""){
+             return
+         }
+         await Axios.post(`${REACT_APP_SERVER_URL}/api/users/tabs/comments`, userData)
+         .then(res=>{console.log(res); getComments();})
+         .catch(err=>{console.log(err)})
 
     }
-   
-    // get comments
     async function getComments  () {
-        console.log('GET COMMMMMMENTS')
+        // console.log('GET COMMMMMMENTS')
         let url = await `${REACT_APP_SERVER_URL}/api/users/tabs/${props.songId}`
+        // console.log(`${REACT_APP_SERVER_URL}/api/users/tabs/${props.songId}`)
          await Axios.get(url).then( 
             async (res)=>{
                 if(res!== undefined){ 
-                    await setComments(res.data.user) 
-                }else{
-                    const placaHolder = [{content : "leave comment"}]
-                    await setComments(placaHolder) 
-
-                }
+                    // console.log('Look below')
+                    // console.log(res.data)
+                    // console.log(url)
+                     await setComments(res.data.user) 
+            }else{
+                const placaHolder = [{content : "leave comment"}]
+                await setComments(placaHolder) 
+                // console.log("33333" + placaHolder)
+            }
         }).catch(err=>{console.log(err)})     
     }
-
     useEffect(()=>{  
         getComments()
     },[])
-
-    // delete
+// function leaveComment () {
     async function deleteComment(e){
-        e.preventDefault()
-        console.log(e.target.value)
-        const userData= {_id: e.target.value,
-                        email: props.email}
-        await Axios.put(`${REACT_APP_SERVER_URL}/api/users/profile/comments/delete`, userData)
-        .then( res=>{
-            console.log(res); 
-            getComments()})
-            .catch(err=>{console.log(err)})
+      e.preventDefault()
+    //   console.log(e.target.value)
+      const userData= {_id: e.target.value,
+                       email: props.email}
+      await Axios.put(`${REACT_APP_SERVER_URL}/api/users/profile/comments/delete`, userData)
+      .then( res=>{
+        //   console.log(res); 
+          getComments()})
+          .catch(err=>{console.log(err)})
     }  
 
     let commentBox = <div>
     </div>
-    let commentOrder;
+let commentOrder;
     const commentArrayAll=[]
     let authorList=commentBox;
-
     if(comments.length !== 0){
         authorList = comments.map((p, index)=>{
-            
             const commentMap = p.comments
-
             const commentList = commentMap.map((b, index)=>{
                 if(b.songsterr_id === props.songId){
                     commentArrayAll.push({
@@ -94,48 +81,44 @@ function Comment (props){
                        id: b._id, 
                        email: b.email
                     })
-                    return <div>
+                        return <div>
                 </div>
                 }
             })
-
             function sortByDate(arr) {
                 arr.sort(function(a,b){
-                   
-                return a.date - b.date;
+                  return a.date - b.date;
                 });
                 console.log(arr)
                 return arr;
-                
-            }
-
+              }
             commentOrder= sortByDate(commentArrayAll).map((a, index)=>{
                 const iddd=`comment${index}`
-                
+
                  return <div>
                    <div className="commentChild1"> <p >   {a.userName}        </p>   </div> 
                    <span>{a.content}</span>
-                    {
-                        a.email===props.email
-                        ?
-                        <button type="button" className="trashButton" value={a.id} key={index}  onClick={deleteComment}>delete</button>
-                        :
-                        <div>
-                        </div>
-                    }
+                   {
+                       a.email===props.email
+                       ?
+                       <button type="button" className="trashButton" value={a.id} key={index}  onClick={deleteComment}>delete</button>
+                       :
+                       <div>
+                           </div>
+                   }
                  </div>
             }) 
-            return <div className="commentsParent" key={index}>
-            </div>
+                return <div className="commentsParent" key={index}>
+               </div>
     })}
-    
     return(
         <div className="commentDisplay">
           <h6>{commentOrder}</h6>
-          <input id="inputComment" type="text" placeholder="Leave a Comment" onClick={(e)=>{e.target.value=''}} onChange={(e=>{setCommentsStore(e.target.value)})}></input>
-          <button  id="comment" onClick={saveComment}>Comment</button>
-        </div> 
+         <form onSubmit={e=>{e.target.reset(); e.preventDefault()}}>
+          <input id="inputComment" type="text" placeholder="Leave a Comment"  onChange={(e=>{setCommentsStore(e.target.value)})}></input>
+          <button  id="comment" onClick={saveComment }>Comment</button>
+          </form>
+        </div>
     )
 }
-
 export default Comment;
