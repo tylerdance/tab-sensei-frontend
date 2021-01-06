747cf2e74798c14301e1f5bf99cbbf3594931f1a
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-01-06 12:55:33 -0500

    last minute home page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,6 +30,22 @@ class Home extends Component {
                 <div className='beatBank'>
                     <BeatBank />
                 </div>
+                <div className="aboutDiv2">
+                    {/* <h2 className="welcome">Welcome to Tab Sensei</h2> */}
+                    <br></br>
+                    <p className="homeText">Tab Sensei allows you to easily search for tabs from Songsterr, practice with beats from our beat bank at your desired tempo, or play along with a YouTube tutorial.</p>
+
+                    <p className="homeText">Save your favorite tabs to your profile and talk to other users in the comments!</p>
+                
+                    <p className="homeText" id="about">
+                        Guitar tablature, or guitar tabs, is a more straightforward way of reading and writing music specifically for guitars. If standard staff notation is the whole book, guitar tabs are the cliff notes. You get all the musical information that you need specifically for the guitar.<br/>
+                    </p>
+                    <p className="homeText">
+                        Even though its simple, guitar tablature is useful as a notation system. You can use it to indicate rhythm, technique, and many other aspects of playing guitar music.
+                    </p>
+                        
+
+                </div>
             </div>
             </div>
             </>
